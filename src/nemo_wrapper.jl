4acc19b6ae994d318638859a57647ac158f4be8d
Taoli Shen <reachiback@gmail.com>
2025-10-20 09:43:31 -0400

    fixed obsolete nemo dependency issue in nemo_wrapper.jl

--- src/nemo_wrapper.jl
+++ src/nemo_wrapper.jl
@@ -1,53 +1,36 @@
+using Nemo
+using AbstractAlgebra          
+using StaticArrays
+
+const FmpzMat = Nemo.MatElem{Nemo.ZZRingElem}
 
 function Base.convert(
-    ::Type{Nemo.fmpz_mat},
-    mat::Union{Matrix{T},MMatrix{N,N,T}},
-) where {N,T<:Real}
-    return Nemo.MatrixSpace(Nemo.ZZ, size(mat)...)(BigInt.(round.(mat)))
+    ::Type{FmpzMat},
+    mat::Union{Matrix{T}, MMatrix{N,N,T}},
+) where {N, T<:Real}
+    return Nemo.matrix(Nemo.ZZ, BigInt.(round.(mat)))
 end
 
-function Base.convert(::Type{MMatrix{N,N,BigInt}}, mat::Nemo.fmpz_mat) where {N}
+function Base.convert(::Type{MMatrix{N,N,BigInt}}, mat::FmpzMat) where {N}
     A = Matrix{BigInt}(mat)
     return convert(MMatrix{N,N,BigInt}, A)
 end
 
-function Base.convert(::Type{Matrix{BigInt}}, mat::Nemo.fmpz_mat)
+function Base.convert(::Type{Matrix{BigInt}}, mat::FmpzMat)
     return Matrix{BigInt}(mat)
 end
-
 
 """
     snf_with_transform(L::Lattice)
     snf_with_transform(mat::MMatrix{M,N}) where {M,N}
     snf_with_transform(mat::Matrix)
 
-
-Wrapper of Nemo.snf_with_transform. Input is converted to BigInt.
+Wrapper of AbstractAlgebra.snf_with_transform. Input is converted to BigInt.
 Returns (S,U,V) such that U*mat*V = S, where S is the Smith normal form of mat.
-
-# Example
-```jldoctest
-julia> using ALFA # hide
-
-julia> using LinearAlgebra # hide
-
-julia> mat = rand(1:10, 10, 10);
-
-julia> (S,U,V) = ALFA.snf_with_transform(mat);
-
-julia> norm(U*mat*V - S) ≈ 0
-true
-
-julia> abs(det(U)) ≈ abs(det(V)) ≈ 1
-true
-
-julia> norm(diagm(diag(S)) - S) ≈ 0
-true
-```
 """
 function snf_with_transform(mat::MMatrix{M,N}) where {M,N}
-    mat = convert(Nemo.fmpz_mat, mat)
-    (S, U, V) = Nemo.snf_with_transform(mat) # U*mat*V = S
+    mat = convert(FmpzMat, mat)
+    (S, U, V) = AbstractAlgebra.snf_with_transform(mat) # U*mat*V = S
     U = convert(MMatrix{size(U)...,BigInt}, U)
     V = convert(MMatrix{size(V)...,BigInt}, V)
     S = convert(MMatrix{size(S)...,BigInt}, S)
@@ -55,14 +38,13 @@ function snf_with_transform(mat::MMatrix{M,N}) where {M,N}
 end
 
 function snf_with_transform(mat::Matrix)
-    mat = convert(Nemo.fmpz_mat, mat)
-    (S, U, V) = Nemo.snf_with_transform(mat) # U*mat*V = S
+    mat = convert(FmpzMat, mat)
+    (S, U, V) = AbstractAlgebra.snf_with_transform(mat) # U*mat*V = S
     U = convert(Matrix{BigInt}, U)
     V = convert(Matrix{BigInt}, V)
     S = convert(Matrix{BigInt}, S)
     return S, U, V
 end
-
 
 """
     hnf(mat::MMatrix{M,N}) where {M, N}
@@ -70,34 +52,16 @@ end
 
 Wrapper of Nemo.hnf. Input is converted to BigInt.
 Returns H = mat*U, s.t. H is in Hermite Normal Form and U is unimodular.
-
-# Example
-```jldoctest
-julia> using ALFA # hide
-
-julia> using LinearAlgebra # hide
-
-julia> mat = rand(1:1000, 2, 2);
-
-julia> H = ALFA.hnf(mat);
-
-julia> norm(LinearAlgebra.tril(H) - H) ≈ 0
-true
-julia> round(abs(det(inv(mat)*H)), digits=5)
-1.0
-```
 """
 function hnf(mat::MMatrix{M,N}) where {M, N}
-    # computes the H = mat*U, s.t. H is in HNF and U is unimodular.
-    mat = convert(Nemo.fmpz_mat, mat)
+    mat = convert(FmpzMat, mat)
     H = transpose(Nemo.hnf(transpose(mat)))
     H = convert(MMatrix{size(H)...,BigInt}, H)
     return H
 end
 
 function hnf(mat::Matrix)
-    # computes the H = mat*U, s.t. H is in HNF and U is unimodular.
-    mat = convert(Nemo.fmpz_mat, mat)
+    mat = convert(FmpzMat, mat)
     H = transpose(Nemo.hnf(transpose(mat)))
     H = convert(Matrix{BigInt}, H)
     return H
@@ -107,28 +71,11 @@ end
     lll(mat::MMatrix{M,N}) where {M, N}
     lll(mat::Matrix)
 
-
 Wrapper of Nemo.lll. Input is converted to BigInt.
-Applies the LLL-Algorithm to the input mat.
-Computes output L, such that mat*T=L for some unimodular T.
-
-# Example
-```jldoctest
-julia> using ALFA # hide
-
-julia> using LinearAlgebra # hide
-
-julia> mat = rand(1:1000, 2, 2);
-
-julia> L = ALFA.lll(mat);
-
-julia> round(abs(det(inv(mat)*L)), digits=5)
-1.0
-```
+Computes L such that mat*T = L for some unimodular T.
 """
 function lll(mat::MMatrix{M,N}) where {M, N}
-    # computes L, such that mat*T = L for some unimodular T
-    mat = convert(Nemo.fmpz_mat, mat)
+    mat = convert(FmpzMat, mat)
     L = Nemo.lll(transpose(mat))
     L = transpose(L)
     L = convert(MMatrix{size(L)...,BigInt}, L)
@@ -136,8 +83,7 @@ function lll(mat::MMatrix{M,N}) where {M, N}
 end
 
 function lll(mat::Matrix)
-    # computes L, such that mat*T = L for some unimodular T
-    mat = convert(Nemo.fmpz_mat, mat)
+    mat = convert(FmpzMat, mat)
     L = Nemo.lll(transpose(mat))
     L = transpose(L)
     L = convert(Matrix{BigInt}, L)
